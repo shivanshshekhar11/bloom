209a69f12d585d517ed5b743d1d438e321c22752
shivanshshekhar11 <shivanshshekhar11@gmail.com>
2022-07-28 19:04:00 +0530

    completed lush UI

--- models/post.js
+++ models/post.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+var moment = require('moment');
+
 var postSchema = new Schema(
     {
         title: {type: String, required: true},
@@ -12,6 +14,18 @@ var postSchema = new Schema(
 
 postSchema.virtual("url").get(function(){
     return "/posts/" + this._id;
+});
+
+postSchema
+.virtual('ago')
+.get(function () {
+
+  const now = new Date();
+  const nowMoment = moment(now);
+  const pastMoment = moment(this.time);
+  const timeAgoString = pastMoment.from(nowMoment); // 2 hours ago
+  return timeAgoString;
+
 });
 
 module.exports = mongoose.model("Post", postSchema);
